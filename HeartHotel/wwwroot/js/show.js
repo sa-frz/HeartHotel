aaaa466ae46c7d8a10c7a2653d83eb21ada8dc6f
Ali <sa.firoozi@gmail.com>
2025-04-28 10:03:27 +0330

    Refactor event titles in Show views for consistency; update CSS for radio button styling and enhance font size adjustment logic in show.js.

--- HeartHotel/wwwroot/js/show.js
+++ HeartHotel/wwwroot/js/show.js
@@ -53,13 +53,24 @@ function setTime() {
     $('#Name').text(programConductors.$values[index].Name);
     $('#Description').text(programConductors.$values[index].Description);
     $('#times').html(`${SaatAz} - ${SaatTa}`);
-    timeLeft = Math.abs(new Date(`1970-01-01T${SaatTa}:00Z`) - new Date(`1970-01-01T${currentTime}Z`)) / 1000;
-    timeLeft--;
-    timeLeft--;
+    timeLeft = Math.abs(new Date(`1970-01-01T${SaatTa}:00Z`) - new Date(`1970-01-01T${currentTime}Z`)) / 1000 - 60;
+
     if (index >= programConductors.$values.length) {
         index = 0;
         return;
     }
     // index++;
     timerInterval = setInterval(updateTimer, 1000);
+}
+
+function adjustFontSizeToFit(elementId) {
+    const element = document.getElementById(elementId);
+    // element.textContent = text;
+    let fontSize = 500; // Start with a base font size
+    element.style.fontSize = fontSize + "px";
+
+    while (element.scrollWidth > element.offsetWidth && fontSize > 10) {
+        fontSize--;
+        element.style.fontSize = fontSize + "px";
+    }
 }
