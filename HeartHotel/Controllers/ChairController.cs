7968a80e5115d2a6df9e7e0e103823492430de3f
Ali <sa.firoozi@gmail.com>
2025-04-28 11:41:05 +0330

    Add Chair management functionality: implement ChairController with CRUD operations, create views for Index, Create, Edit, Details, and Delete actions, and update project dependencies for Entity Framework Core.

---
+++ HeartHotel/Controllers/ChairController.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HeartHotel.Models;
+
+namespace HeartHotel.Controllers
+{
+    public class ChairController : Controller
+    {
+        private readonly EventisContext _context;
+
+        public ChairController(EventisContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ChairController
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Chairs.ToListAsync());
+        }
+
+        // GET: ChairController/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chair = await _context.Chairs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (chair == null)
+            {
+                return NotFound();
+            }
+
+            return View(chair);
+        }
+
+        // GET: ChairController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ChairController/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Post,Cv")] Chair chair, [FromForm] IEnumerable<IFormFile> Image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(chair);
+                await _context.SaveChangesAsync();
+
+                if (Image.Count() > 0)
+                {
+                    var validFile = Image.FirstOrDefault();
+                    // string fs = DateTime.Now.ToPersianDateTime().Replace("/", "").Replace(" ", "-").Replace(":", "") + "-" + validFile.Length;
+                    // fs = fs.Replace(" ", "-").Replace("?", "");
+                    // var strFilename = (fs + Path.GetExtension(validFile.FileName)).ToLower();
+                    var dir = Path.Combine(
+                          Directory.GetCurrentDirectory(),
+                          "wwwroot", "Files", "Chairs",
+                          chair.Id.ToString().Trim());
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+
+                    var path = Path.Combine(dir, chair.Image!.Trim());
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        validFile?.CopyTo(stream);
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(chair);
+        }
+
+        // GET: ChairController/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chair = await _context.Chairs.FindAsync(id);
+            if (chair == null)
+            {
+                return NotFound();
+            }
+            return View(chair);
+        }
+
+        // POST: ChairController/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Post,Image,Cv")] Chair chair)
+        {
+            if (id != chair.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(chair);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ChairExists(chair.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(chair);
+        }
+
+        // GET: ChairController/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var chair = await _context.Chairs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (chair == null)
+            {
+                return NotFound();
+            }
+
+            return View(chair);
+        }
+
+        // POST: ChairController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var chair = await _context.Chairs.FindAsync(id);
+            if (chair != null)
+            {
+                _context.Chairs.Remove(chair);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ChairExists(int id)
+        {
+            return _context.Chairs.Any(e => e.Id == id);
+        }
+    }
+}
